35d5cb54d5df16aacabd6b2adc181e52921ed4c2
Jackie Tsien <jackie10673311@gmail.com>
2023-11-27 11:01:21 +0800

    add eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,32 @@
 module.exports = {
-  env: {},
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
